fbd5c39aa77a52168cc3eca4df723af56876ed13
Vikesh Khanna <khanna.vikesh@gmail.com>
2012-07-29 02:31:15 +0530

    Performance impv. - Moving to background page

--- js/content_script.js
+++ js/content_script.js
@@ -1,6 +1,5 @@
 (function(){
 	var users = new Array();
-	var cache = {};
 	var hover_menu_div, hover_menu_contents_div;
 	var hover_menu_css = 'horcrux_menu';
 	var hover_menu_contents_css = 'horcrux_menu_contents';
@@ -73,104 +72,105 @@
 			hover_menu_contents_div.removeChild(hover_menu_contents_div.firstChild);
 		}
 		
-		if(cache.hasOwnProperty(username))
-		{
-			var img = cache[username]["img"];
+		chrome.extension.sendRequest({'method' : 'getUser', 'username': username, 'name': name}, function(response){
+			var data = response["data"];
 			
-			var name_h1 = document.createElement('h1');
-			name_h1.innerHTML = name;
-			
-			var header = document.createElement("div");
-			$(header).addClass('hover_header');
-			
-			header.appendChild(name_h1);
-		
-			if(cache[username]["intro"])
+			if(data)
 			{
-				var intro = document.createElement("h2");
-				intro.innerHTML = cache[username]["intro"];
-				header.appendChild(intro);
+				var img = data["img"];
+				var name_h1 = document.createElement('h1');
+				name_h1.innerHTML = name;
+				
+				var header = document.createElement("div");
+				$(header).addClass('hover_header');
+				header.appendChild(name_h1);
+			
+				if(data["intro"])
+				{
+					var intro = document.createElement("h2");
+					intro.innerHTML = data["intro"];
+					header.appendChild(intro);
+				}
+			
+				var img_div = document.createElement('div');
+				$(img_div).addClass('image');
+				img_tag = document.createElement('img');
+				img_tag.src = img;
+				$(img_tag).addClass('hover_image');
+				img_div.appendChild(img_tag);
+				
+				var info_div = document.createElement('div');
+				$(info_div).addClass('info');
+				
+				var table = document.createElement('table');
+				table.innerHTML = "<tr> \
+										<td><h2>" + data["followers"] +"</h2> <h3>followers</h3></td> \
+										<td><h2>" + data["following"] +"</h2> <h3>following</h3></td> \
+									</tr> \
+									<tr>  \
+										<td><h2>" + data["topics"] +"</h2> <h3>topics</h3></td> \
+										<td><h2>" + data["boards"] +"</h2> <h3>boards</h3></td> \
+									</tr>";
+				
+				info_div.appendChild(table);
+				
+				var follow = document.createElement('div');
+				$(follow).addClass('horcrux_follow');
+				
+				// follow button
+				var span = document.createElement('span');
+				$(span).addClass('quora-follow-button');
+				span.setAttribute("data-name", username);
+				span.appendChild(document.createTextNode('Loading Follow Status'));
+				
+				script = document.createElement('script');
+				script.src = "http://qsc.cf.quoracdn.net/-c20a67afaf9f0d3a.js?embed_code=UnYGxfU";
+				span.appendChild(script);
+				follow.appendChild(span);
+				
+				var snippet = document.createElement('div');
+				$(snippet).addClass('horcrux_contents_snippet');
+				
+				snippet.appendChild(img_div);
+				snippet.appendChild(info_div);
+				
+				hover_menu_contents_div.appendChild(header);
+				hover_menu_contents_div.appendChild(document.createElement('hr'));
+				hover_menu_contents_div.appendChild(snippet);
+				hover_menu_contents_div.appendChild(document.createElement('hr'));
+				hover_menu_contents_div.appendChild(follow);
 			}
-		
-			var img_div = document.createElement('div');
-			$(img_div).addClass('image');
-			img_tag = document.createElement('img');
-			img_tag.src = img.src;
-			$(img_tag).addClass('hover_image');
-			img_div.appendChild(img_tag);
-			
-			var info_div = document.createElement('div');
-			$(info_div).addClass('info');
-			
-			var table = document.createElement('table');
-			table.innerHTML = "<tr> \
-									<td><h2>" + cache[username]["followers"] +"</h2> <h3>followers</h3></td> \
-									<td><h2>" + cache[username]["following"] +"</h2> <h3>following</h3></td> \
-								</tr> \
-								<tr>  \
-									<td><h2>" + cache[username]["topics"] +"</h2> <h3>topics</h3></td> \
-									<td><h2>" + cache[username]["boards"] +"</h2> <h3>boards</h3></td> \
-								</tr>";
-			
-			info_div.appendChild(table);
-			
-			var follow = document.createElement('div');
-			$(follow).addClass('horcrux_follow');
-			
-			// follow button
-			var span = document.createElement('span');
-			$(span).addClass('quora-follow-button');
-			span.setAttribute("data-name", username);
-			span.appendChild(document.createTextNode('Loading Follow Status'));
-			
-			script = document.createElement('script');
-			script.src = "http://qsc.cf.quoracdn.net/-c20a67afaf9f0d3a.js?embed_code=UnYGxfU";
-			span.appendChild(script);
-			follow.appendChild(span);
-			
-			var snippet = document.createElement('div');
-			$(snippet).addClass('horcrux_contents_snippet');
-			
-			snippet.appendChild(img_div);
-			snippet.appendChild(info_div);
-			
-			hover_menu_contents_div.appendChild(header);
-			hover_menu_contents_div.appendChild(document.createElement('hr'));
-			hover_menu_contents_div.appendChild(snippet);
-			hover_menu_contents_div.appendChild(document.createElement('hr'));
-			hover_menu_contents_div.appendChild(follow);
-		}
-		else
-		{
-			$(hover_menu_contents_div).html('Still loading data. Please hover in a while :(	');
-		}
-		
-		curUser = user;
-		
-		if(hoverState == states.DISPLAY_HIDE_INVOKE)
-		{
-			hoverState = states.DISPLAY_SHOW_INVOKE;
-			showCard();
-		}
-		else
-		{
-			// wait before showing
-			hoverState = states.DISPLAY_SHOW_INVOKE;
-			setTimeout( function(){
-					showCard();
-				}, 800);
-		}
-		
-		function showCard()
-		{
-			if(curUser == user && hoverState == states.DISPLAY_SHOW_INVOKE)
+			else
 			{
-					hoverState = states.DISPLAY_ON;
-					$(hover_menu_div).removeClass('hidden');
-					$(hover_menu_div).offset({top: $(user).offset().top + $(user).outerHeight(), left: $(user).offset().left});
-					$(hover_menu_div).animate({opacity:1}, 300, function() { } );
+				$(hover_menu_contents_div).html('Still loading data. Please hover in a while :(	');
 			}
-		}
+			
+			curUser = user;
+			if(hoverState == states.DISPLAY_HIDE_INVOKE)
+			{
+				hoverState = states.DISPLAY_SHOW_INVOKE;
+				showCard();
+			}
+			else
+			{
+				// wait before showing
+				hoverState = states.DISPLAY_SHOW_INVOKE;
+				setTimeout( function(){
+						showCard();
+					}, 800);
+			}
+			
+			function showCard()
+			{
+				if(curUser == user && hoverState == states.DISPLAY_SHOW_INVOKE)
+				{
+						hoverState = states.DISPLAY_ON;
+						$(hover_menu_div).removeClass('hidden');
+						$(hover_menu_div).offset({top: $(user).offset().top + $(user).outerHeight(), left: $(user).offset().left});
+						$(hover_menu_div).animate({opacity:1}, 300, function() { } );
+				}
+			}
+		});
 	}
 	
 	function hideHoverMenu()
@@ -226,7 +226,8 @@
 		if(users.indexOf(user) == -1)
 		{
 			var name = escapeString(user.innerHTML);
-			var username= getUsername(user);
+			var username = getUsername(user);
+			
 			console.log("<horcrux> adding new tag for user " + name);
 			users.push(user);
 			$(user).mouseenter(function(){ onMouseEnter(this); });
@@ -245,68 +246,10 @@
 				}
 			});
 			
-			// update cache
-			if(!cache.hasOwnProperty(username))
-			{
-				console.log("<horcrux> Adding cache entry for : " + name);
-				fetchCacheData(user);
-			}
+			// send request to background page
+			chrome.extension.sendRequest({"method":"addUser", "username": username }, function(response){
+				console.log("<horcrux> Successfully added user : " + user.innerHTML);
+			});
 		}
 	}
-	
-	// fetch data
-	function fetchCacheData(user)
-	{
-		var userName = getUsername(user);
-		var name = escapeString(user.innerHTML);
-		
-		var img;
-		var followers, following, topics, boards;
-		var data;
-		
-		$.ajax({
-			url: user.href,
-			dataType:'html',
-			success: function(response)
-			{
-				try
-				{
-					img = $(response).find("img.profile_photo_img[alt='"+ name +"']")[0];
-					
-					if(!img)
-					{
-						img = $(response).find("img.profile_photo_img[width='200']")[0];
-					}
-					
-					followers = $(response).find("a[href|='/"+userName+"/followers'] > span")[0];
-					following = $(response).find("a[href|='/"+userName+"/following'] > span")[0];
-					topics = $(response).find("a[href|='/"+userName+"/topics'] > span")[0];
-					boards = $(response).find("a[href|='/"+userName+"/boards'] > span")[0];
-					intro = $(response).find(".rep")[1];
-				}
-				catch(e)
-				{
-					console.log("<horcux> [error] for user : " + user.href  + ", " + e );
-				}
-				
-				data = {
-				"name": name,
-				"img":img,
-				"followers":followers == undefined ? 0 : followers.innerHTML,
-				"following":following == undefined ? 0 : following.innerHTML,
-				"topics":topics == undefined ? 0 : topics.innerHTML,
-				"boards":boards == undefined ? 0 : boards.innerHTML,
-				"intro" : intro == undefined ? undefined : intro.innerHTML 
-				};
-				
-				cache[userName] = data;
-			},
-			error : function(xhr, textStatus, errorThrown)
-			{
-				console.log("<horcrux> " + textStatus + " = " + errorThrown);
-			}
-		});	
-	}
-	
-	// image cache
 })();
